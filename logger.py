5e5d1b56c9c401cb59a9094fa9d033d4dea5f7fc
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2018-12-11 18:49:33 +0900

    add basic config. modify output level.

--- logger.py
+++ logger.py
@@ -3,5 +3,6 @@ import logging
 
 if __name__ == '__main__':
     print('hi')
+    logging.basicConfig(level=logging.INFO)
     logging.info('Logging Info Test.')
     logging.warning('Logging Warning Test')
