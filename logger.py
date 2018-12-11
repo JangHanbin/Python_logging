361e5216da2d205776584a0ec5caa88370e129c3
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2018-12-11 18:52:13 +0900

    Logging level test. Log Level \'Debug > Info > Warning > Error > Critical\'

--- logger.py
+++ logger.py
@@ -1,8 +1,13 @@
 import logging
 
 
+# Logging level Debug > Info > Warning > Error > Critical
 if __name__ == '__main__':
     print('hi')
     logging.basicConfig(level=logging.INFO)
+    logging.debug('Loggin Debug Test')
     logging.info('Logging Info Test.')
     logging.warning('Logging Warning Test')
+    logging.error('Logging Error Test')
+    logging.critical('Logging Critical Test')
+    
