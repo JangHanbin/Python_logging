169ee77f996ef9b5ae339324cb159872d586e762
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2018-12-11 18:55:38 +0900

    logging file output test.

--- logger.py
+++ logger.py
@@ -4,10 +4,11 @@ import logging
 # Logging level Debug > Info > Warning > Error > Critical
 if __name__ == '__main__':
     print('hi')
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(filename='./test.log', level=logging.INFO)
     logging.debug('Loggin Debug Test')
-    logging.info('Logging Info Test.')
+    logging.info('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Logging File Output Test@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    logging.info('Log Info Test')
     logging.warning('Logging Warning Test')
     logging.error('Logging Error Test')
     logging.critical('Logging Critical Test')
-    
+    logging.info('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Logging File Output Test@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
